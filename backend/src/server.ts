a4d9b5a3b24b222b8d7578bd114b3b184db48ab7
gianluqqa <gianlucacaravone55@gmail.com>
2025-10-01 20:38:57 -0300

    Inicializacion del proyecto Booker

---
+++ backend/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Booker Backend funcionando ✅');
+});
+
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
